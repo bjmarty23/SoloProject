88d6054f8b54c23e3348d2dd67826cb1a27d4537
Brittany Marty <bjmarty23@gmail.com>
2018-11-15 19:45:56 -0600

    new location in a card

--- src/components/NewLocation/NewLocation.js
+++ src/components/NewLocation/NewLocation.js
@@ -8,6 +8,8 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import ReplyIcon from '@material-ui/icons/Reply'
 import LocationSearchingIcon from '@material-ui/icons/LocationSearching'
+import Card, { CardContent, } from 'material-ui/Card';
+import { Grid } from 'material-ui';
 
 
 //gives you access to store 
@@ -17,22 +19,22 @@ const mapStateToProps = state => ({
 });
 
 class NewLocation extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state={
+    this.state = {
       latitude: '',
-      longitude:'',
+      longitude: '',
       name: '',
-      type:'',
+      type: '',
       notes: '',
     }
-  }  
+  }
   logout = () => {
     this.props.dispatch(triggerLogout());
     // this.props.history.push('home');
   }
-  AddLocation = (newType) =>{
+  AddLocation = (newType) => {
     // event.preventDefault();
     console.log('clicked', this.state)
     this.props.dispatch({
@@ -41,26 +43,26 @@ class NewLocation extends Component {
     })
     console.log(this.state)
   }
- 
+
   handleInputChangeFor = propertyName => (event) => {
     this.setState({
       ...this.state,
       [propertyName]: event.target.value,
       ...this.state.longitude,
-      [propertyName]: event.target.value, 
+      [propertyName]: event.target.value,
       ...this.state.latitude,
       [propertyName]: event.target.value,
       ...this.state.name,
-      [propertyName]: event.target.value, 
+      [propertyName]: event.target.value,
       ...this.state.type,
-      [propertyName]: event.target.value, 
+      [propertyName]: event.target.value,
       ...this.state.notes,
       [propertyName]: event.target.value,
     });
   }
   //write funtionality to sync to device location
-  locate = () =>{
-    console.log('locate button clicked',this.state)
+  locate = () => {
+    console.log('locate button clicked', this.state)
   }
 
   render() {
@@ -68,61 +70,71 @@ class NewLocation extends Component {
 
     if (this.props.user.userName) {
       content = (
-          <div className="newlocation">
-          <select className="newLocationDropDown" 
-          // value={this.state.type}
-          name="type" onChange={this.handleInputChangeFor('type')}>
-              <option value="pick here">Type:</option>
-              <option value="Restroom">Restroom</option>
-              <option value="Fountain">Water Fountain</option>
-              <option value="Restaurant">Restaurant</option>
-              </select><br />
-            Name:<input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handleInputChangeFor('name')}
-              /> <br /> 
-            <textarea rows="2" 
-            cols="40" 
+        <div className="newlocation">
+          <select className="newLocationDropDown"
+            // value={this.state.type}
+            name="type" onChange={this.handleInputChangeFor('type')}>
+            <option value="pick here">Type:</option>
+            <option value="Restroom">Restroom</option>
+            <option value="Fountain">Water Fountain</option>
+            <option value="Restaurant">Restaurant</option>
+          </select><br />
+          Name:<input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChangeFor('name')}
+          /> <br />
+          <textarea rows="2"
+            cols="40"
             type="notes"
-            name="comment" 
+            name="comment"
             value={this.state.notes}
             placeholder="Enter text here.."
             onChange={this.handleInputChangeFor('notes')}
-            form="usrform">dsf</textarea> 
-            <br />                    
-            Lat:<input
-                type="latitude"
-                name="latitude"
-                value={this.state.latitude}
-                onChange={this.handleInputChangeFor('latitude')}
-              /> <br /> 
-              Long:<input
-                type="longitude"
-                name="longitude"
-                value={this.state.longitude}
-                onChange={this.handleInputChangeFor('longitude')}
-              /> 
-                                 
-          </div>
+            form="usrform">dsf</textarea>
+          <br />
+          Lat:<input
+            type="latitude"
+            name="latitude"
+            value={this.state.latitude}
+            onChange={this.handleInputChangeFor('latitude')}
+          /> <br />
+          Long:<input
+            type="longitude"
+            name="longitude"
+            value={this.state.longitude}
+            onChange={this.handleInputChangeFor('longitude')}
+          />
+
+        </div>
       );
     }
 
     return (
       <div>
-        
-         <Link to="/home">
-        <Button size="mini" variant="fab" color="primary" aria-label="add"  onClick={this.back}><ReplyIcon /></Button>
+
+        <Link to="/home">
+          <Button size="mini" variant="fab" color="primary" aria-label="add" onClick={this.back}><ReplyIcon /></Button>
         </Link>
-          <h1
-            id="newLocation" 
-          >
-            Add new water source!
-          </h1>
-        { content }
-        <Link to="/home"><Button variant="fab" color="primary" aria-label="add" size="small"  onClick={this.AddLocation}><AddIcon /></Button></Link>
-              <Button variant="fab" color="primary" aria-label="add" size="small" onClick={this.locate}><LocationSearchingIcon /></Button> 
+       
+          <Grid item>
+            <Card className="card" style={{ margin: "20px" }} >
+              <CardContent>
+                Add new water source!
+         
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card className="card" style={{ margin: "20px" }} >
+              <CardContent>
+                {content}
+              </CardContent>
+            </Card>
+          </Grid>
+        <Link to="/home"><Button variant="fab" color="primary" aria-label="add" size="small" onClick={this.AddLocation}><AddIcon /></Button></Link>
+        <Button variant="fab" color="primary" aria-label="add" size="small" onClick={this.locate}><LocationSearchingIcon /></Button>
       </div>
     );
   }
